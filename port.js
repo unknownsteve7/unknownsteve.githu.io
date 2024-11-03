54f18c83f92cc7c24bb30f02c39cf900fc5e1fa8
Mohan Madicharla <163634617+unknownsteve7@users.noreply.github.com>
2024-11-03 11:04:19 +0530

    Initial commit

---
+++ port.js
@@ -0,0 +1,14 @@
+function start(x){
+    x.style.color="aqua";
+    x.style.border="2px solid pink";
+    x.style.borderRadius="5px";
+    x.style.borderWidth="thin";
+    x.style.backgroundColor="pink";
+}
+function stop(x){
+    x.style.color="white";
+    x.style.border="None";
+    x.borderRadius="100px"
+    x.style.borderWidth="thick";
+    x.style.backgroundColor="gray";
+}
